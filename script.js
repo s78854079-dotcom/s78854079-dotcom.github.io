407b286b3d1479e37d8d0a13c1f10557700bca68
s78854079-dotcom <s78854079@gmail.com>
2026-07-15 20:59:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -138,3 +138,16 @@ window.loginWithPi = function () {
     );
 
 }
+function loginPi(){
+
+    Pi.authenticate(
+        ["username"],
+        function(auth){
+            alert("تم تسجيل الدخول: " + auth.user.username);
+        },
+        function(error){
+            alert("خطأ: " + error);
+        }
+    );
+
+}
